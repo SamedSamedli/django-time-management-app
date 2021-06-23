f5b3de15df071b235ebca3bafd8c7c665db53111
Samed Samedli <samedli.ssi@gmail.com>
2021-06-23 20:58:22 +0400

    'First-Commit'

---
+++ app/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from app import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('login/', views.login, name="login"),
+    path('signup/', views.signup),
+    path('logout/', views.signout),
+    path('add-todo/', views.add_todo),
+    path('delete-todo/<int:id>', views.delete_todo),
+    path('change-status/<int:id>/<str:status>', views.change_todo),
+]
---
+++ app/views.py
@@ -0,0 +1,100 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login as loginUser, logout
+
+# from .forms import UserForm
+
+
+# Create your views here.
+from app.forms import TODOForm
+from app.models import TODO
+from django.contrib.auth.decorators import login_required
+
+
+@login_required(login_url='login')
+def home(request):
+    if request.user.is_authenticated:
+        user = request.user
+        form = TODOForm()
+        todos = TODO.objects.filter(user=user).order_by('priority')
+        return render(request, 'index.html', context={'form': form, 'todos': todos})
+
+
+def login(request):
+    if request.method == 'GET':
+        form = AuthenticationForm()
+        context = {
+            "form": form
+        }
+        return render(request, 'login.html', context=context)
+    else:
+        form = AuthenticationForm(data=request.POST)
+        print(form.is_valid())
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                loginUser(request, user)
+                return redirect('home')
+        else:
+            context = {
+                "form": form
+            }
+            return render(request, 'login.html', context)
+
+
+def signup(request):
+    if request.method == 'GET':
+        form = UserCreationForm()
+        context = {
+            "form": form
+        }
+        return render(request, 'signup.html', context=context)
+    else:
+        print(request.POST)
+        form = UserCreationForm(request.POST)
+        context = {
+            "form": form
+        }
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                return redirect('login')
+        else:
+            return render(request, 'signup.html', context=context)
+
+
+@login_required(login_url='login')
+def add_todo(request):
+    if request.user.is_authenticated:
+        user = request.user
+        print(user)
+        form = TODOForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            todo = form.save(commit=False)
+            todo.user = user
+            todo.save()
+            print(todo)
+            return redirect("home")
+        else:
+            return render(request, 'index.html', context={'form': form})
+
+
+def signout(request):
+    logout(request)
+    return redirect('login')
+
+
+def delete_todo(request, id):
+    print(id)
+    TODO.objects.get(pk=id).delete()
+    return redirect('home')
+
+
+def change_todo(request, id, status):
+    todo = TODO.objects.get(pk=id)
+    todo.status = status
+    todo.save()
+    return redirect('home')
